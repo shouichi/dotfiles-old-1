65447cdf025f13556160d7a916150075988da473
Shouichi Kamiya <shouichi.kamiya@gmail.com>
2021-08-10 12:07:53 +0900

    Define func M to switch to master and delete the last branch

--- .zsh/80-func.zsh
+++ .zsh/80-func.zsh
@@ -2,3 +2,8 @@ ghq-fzf-cd() {
   declare -r REPO_NAME="$(ghq list >/dev/null | fzf-tmux --reverse +m)"
   [[ -n "${REPO_NAME}" ]] && cd "$(ghq root)/${REPO_NAME}"
 }
+
+M() {
+  declare -r BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+  git checkout master && git branch -D $BRANCH && git pull
+}
